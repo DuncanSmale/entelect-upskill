2f055f2b39aa23235f18ff83b80621f7df3d949e
duncan.smale <duncan.smale@entelect.co.za>
2023-03-27 09:51:21 +0200

    Added test lombok dependency.

--- src/test/java/com/entelect/upskill/configuration/PersonConfiguration.java
+++ src/test/java/com/entelect/upskill/configuration/PersonConfiguration.java
@@ -1,28 +1,16 @@
 package com.entelect.upskill.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "person")
+@Getter
+@Setter
 public class PersonConfiguration {
     private String name;
     private int age;
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
