36c5a014710d173eaf2e032d1bd30dbfae43b111
duncan.smale <duncan.smale@entelect.co.za>
2023-04-03 10:46:09 +0200

    Refactored book controller tests to use abstract controller test base class.

--- src/test/java/com/entelect/upskill/library/controller/book/BookControllerGetAllTest.java
+++ src/test/java/com/entelect/upskill/library/controller/book/BookControllerGetAllTest.java
@@ -1,12 +1,10 @@
 package com.entelect.upskill.library.controller.book;
 
+import com.entelect.upskill.library.common.AbstractControllerTest;
 import com.entelect.upskill.library.dtos.BookDTO;
 import com.entelect.upskill.library.repository.BookRepository;
 import com.entelect.upskill.properties.PersonProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,10 +12,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -28,57 +26,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("testing")
 @SpringBootTest
 @AutoConfigureMockMvc
-class BookControllerGetAllTest {
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    @Autowired
-    private MockMvc mockMvc;
+class BookControllerGetAllTest extends AbstractControllerTest {
+
     @Autowired
     private PersonProperties testConfiguration;
     @MockBean
     private BookRepository bookRepository;
 
-    private String getUri() {
+    @Override
+    protected void mockClientResponse() {
+        when(bookRepository.findAll()).thenReturn(testConfiguration.getBooks());
+    }
+
+    protected String getUri() {
         return "http://localhost/book";
     }
 
-    @BeforeEach
-    void setup() {
-        objectMapper.findAndRegisterModules();
+    @Override
+    protected String stubRequestAsString() throws JsonProcessingException {
+        return null;
     }
 
-    @Test
-    @DisplayName("Given a REST request, " +
-            "when the REST call is successful, " +
-            "then I expect the response to be correct")
-    public void testSuccessfulResponse() throws Exception {
-        // Given
-        mockRepositoryBehaviour();
+    protected void verifyResponse(MvcResult result) throws IOException {
+        BookDTO[] response = jsonMapper.readValue(result.getResponse().getContentAsString(), BookDTO[].class);
+        assertNotNull(response);
+        assertEquals("Happy Peter and the Wizard of Escabar", response[0].getTitle());
+        assertEquals("Penguin Books", response[0].getPublisher());
+        assertEquals(LocalDate.of(2021, 1, 3), response[0].getPublishedDate());
+        assertEquals("0-2487-9445-0", response[0].getISBN());
+        assertEquals(1, response[0].getAuthorId());
+    }
 
-        // When
-        MvcResult result = mockMvc.perform(
+    @Override
+    protected MvcResult performRestRequest() throws Exception {
+        return mockMvc.perform(
                         get(getUri()).contentType(MediaType.APPLICATION_JSON)
                                 .headers(new HttpHeaders()))
                 .andExpect(status().isOk())
                 .andReturn();
-
-        // Then
-        assertNotNull(result);
-        verifyResponse(result);
-    }
-
-    private void mockRepositoryBehaviour() {
-        when(bookRepository.findAll()).thenReturn(testConfiguration.getBooks());
-    }
-
-    private void verifyResponse(MvcResult result) throws IOException {
-        BookDTO[] response = objectMapper.readValue(result.getResponse().getContentAsString(), BookDTO[].class);
-        assertNotNull(response);
-        assertEquals("Happy Peter and the Wizard of Escabar", response[0].getTitle());
-        assertEquals("Penguin Books", response[0].getPublisher());
-        assertEquals("2021-01-03", response[0].getPublishedDate());
-        assertEquals("0-2487-9445-0", response[0].getISBN());
-        assertEquals(1, response[0].getAuthorId());
-
     }
 }
 
--- src/test/java/com/entelect/upskill/library/controller/book/BookControllerGetSingleTest.java
+++ src/test/java/com/entelect/upskill/library/controller/book/BookControllerGetSingleTest.java
@@ -1,12 +1,10 @@
 package com.entelect.upskill.library.controller.book;
 
+import com.entelect.upskill.library.common.AbstractControllerTest;
 import com.entelect.upskill.library.dtos.BookDTO;
 import com.entelect.upskill.library.repository.BookRepository;
 import com.entelect.upskill.properties.PersonProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,10 +12,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,57 +28,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("testing")
 @SpringBootTest
 @AutoConfigureMockMvc
-class BookControllerGetSingleTest {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+class BookControllerGetSingleTest extends AbstractControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
     @Autowired
     private PersonProperties testConfiguration;
     @MockBean
     private BookRepository bookRepository;
 
-    private String getUri() {
+    @Override
+    protected void mockClientResponse() {
+        when(bookRepository.findById(anyInt())).thenReturn(Optional.ofNullable(testConfiguration.getBooks().get(0)));
+    }
+
+    protected String getUri() {
         return "http://localhost/book/1";
     }
 
-    @BeforeEach
-    void setup() {
-        objectMapper.findAndRegisterModules();
+    @Override
+    protected String stubRequestAsString() throws JsonProcessingException {
+        return null;
     }
 
-    @Test
-    @DisplayName("Given a REST request, " +
-            "when the REST call is successful, " +
-            "then I expect the response to be correct")
-    public void testSuccessfulResponse() throws Exception {
-        // Given
-        mockRepositoryBehaviour();
+    protected void verifyResponse(MvcResult result) throws IOException {
+        BookDTO response = jsonMapper.readValue(result.getResponse().getContentAsString(), BookDTO.class);
+        assertNotNull(response);
+        assertEquals("Happy Peter and the Wizard of Escabar", response.getTitle());
+        assertEquals("Penguin Books", response.getPublisher());
+        assertEquals(LocalDate.of(2021, 1, 3), response.getPublishedDate());
+        assertEquals("0-2487-9445-0", response.getISBN());
+        assertEquals(1, response.getAuthorId());
+    }
 
-        // When
-        MvcResult result = mockMvc.perform(
+    @Override
+    protected MvcResult performRestRequest() throws Exception {
+        return mockMvc.perform(
                         get(getUri()).contentType(MediaType.APPLICATION_JSON)
                                 .headers(new HttpHeaders()))
                 .andExpect(status().isOk())
                 .andReturn();
-
-        // Then
-        assertNotNull(result);
-        verifyResponse(result);
-    }
-
-    private void mockRepositoryBehaviour() {
-        when(bookRepository.findById(anyInt())).thenReturn(Optional.ofNullable(testConfiguration.getBooks().get(0)));
-    }
-
-    private void verifyResponse(MvcResult result) throws IOException {
-        BookDTO response = objectMapper.readValue(result.getResponse().getContentAsString(), BookDTO.class);
-        assertNotNull(response);
-        assertEquals("Happy Peter and the Wizard of Escabar", response.getTitle());
-        assertEquals("Penguin Books", response.getPublisher());
-        assertEquals("2021-01-03", response.getPublishedDate());
-        assertEquals("0-2487-9445-0", response.getISBN());
-        assertEquals(1, response.getAuthorId());
     }
 }
 
